ecfd7f18f3c40cff5517302721a051d9570d0f5f
Amartya Varma <amartya.varma@gmail.com>
2024-04-16 20:24:45 +0100

    d

--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 from keras.models import load_model
 from tcn import TCN 
-# from transformers import Wav2Vec2Processor, Wav2Vec2Model
-# import torch
+from transformers import Wav2Vec2Processor, Wav2Vec2Model
+import torch
 from keras.models import Sequential
 from tensorflow.keras.layers import Dense
 
 # Manually recreate the architecture
 input_shape = (None, 768)  # None signifies variable sequence lengths, 768 might be your feature dimension
 output_units = 5
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 model = Sequential([
     TCN(input_shape=input_shape,
@@ -25,16 +27,17 @@ model = Sequential([
 print(model.summary())
 
 
+
 # Load the weights
 model.load_weights('model_weights.h5')
 
-# print("model weights loaded")
-# processor = Wav2Vec2Processor.from_pretrained('facebook/wav2vec2-base-960h')
-# model = Wav2Vec2Model.from_pretrained('facebook/wav2vec2-base-960h')
-# model = model.to(device)
-# inputs = processor(resampled_audio, sampling_rate=16000, return_tensors="pt", padding=True)
-# inputs = inputs.to(device)
-# with torch.no_grad():
-#     outputs = model(**inputs)
-# output_tensor = outputs.last_hidden_state
-# print(output_tensor)
+print("model weights loaded")
+processor = Wav2Vec2Processor.from_pretrained('facebook/wav2vec2-base-960h')
+aud_model = Wav2Vec2Model.from_pretrained('facebook/wav2vec2-base-960h')
+aud_model = aud_model.to(device)
+inputs = processor(resampled_audio, sampling_rate=16000, return_tensors="pt", padding=True)
+inputs = inputs.to(device)
+with torch.no_grad():
+    outputs = aud_model(**inputs)
+output_tensor = outputs.last_hidden_state
+print(output_tensor)
