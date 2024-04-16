fcc266c5ccf110cb971f86d797323dabdf80826a
Amartya Varma <amartya.varma@gmail.com>
2024-04-16 21:02:33 +0100

    d

--- utils/get_bins.py
+++ utils/get_bins.py
@@ -4,11 +4,12 @@ import numpy as np
 
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+processor = Wav2Vec2Processor.from_pretrained('facebook/wav2vec2-base-960h')
+aud_model = Wav2Vec2Model.from_pretrained('facebook/wav2vec2-base-960h')
+aud_model = aud_model.to(device)
 
 def get_output_tensor(audio):
-    processor = Wav2Vec2Processor.from_pretrained('facebook/wav2vec2-base-960h')
-    aud_model = Wav2Vec2Model.from_pretrained('facebook/wav2vec2-base-960h')
-    aud_model = aud_model.to(device)
+
     inputs = processor(audio, sampling_rate=16000, return_tensors="pt", padding=True)
     inputs = inputs.to(device)
     with torch.no_grad():
--- utils/load_wav_as_numpy.py
+++ utils/load_wav_as_numpy.py
@@ -1,38 +1,7 @@
 import wave
 import numpy as np
+import librosa
 
 def load_wav_as_numpy(filename):
-    # Open the WAV file
-    with wave.open(filename, 'r') as wav_file:
-        # Extract Audio Parameters
-        n_channels = wav_file.getnchannels()
-        n_frames = wav_file.getnframes()
-        sample_width = wav_file.getsampwidth()
-        framerate = wav_file.getframerate()
-
-        # Read audio frames as bytes
-        audio_frames = wav_file.readframes(n_frames)
-
-        # Convert bytes to numpy array based on sample width
-        if sample_width == 1:
-            # 8-bit audio
-            dtype = np.uint8
-        elif sample_width == 2:
-            # 16-bit audio
-            dtype = np.int16
-        elif sample_width == 4:
-            # 32-bit audio
-            dtype = np.int32
-        else:
-            raise ValueError("Unsupported sample width")
-
-        # Create a numpy array from bytes, using the correct data type
-        data = np.frombuffer(audio_frames, dtype=dtype)
-
-        # If stereo, convert shape to (n_frames, n_channels)
-        if n_channels > 1:
-            data = np.reshape(data, (n_frames, n_channels))
-
-    return data
-
-
+    audio, sr = librosa.load(filename, sr=16000)
+    return audio
